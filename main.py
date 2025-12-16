4efedc1b296a508fa81a11710f25c11d3d1cc941
ryomaueda <homerun.ryo.628@gmail.com>
2025-12-16 22:50:23 +0900

    add system prompt

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ load_dotenv()
 app = FastAPI()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+SYSTEM_PTOMPT = """
+あなたはLLMとWeb APIに詳しいソフトウェアエンジニアです。
+初心者にも理解できるよう、複雑な概念を噛み砕いて説明することが得意です。
+
+あなたの目的は、ユーザーの技術的な質問に対して、
+「なぜそうなるのか」が理解できる回答を返すことです。
+
+以下の制約を必ず守ってください。
+- 回答は必ず日本語で行うこと
+- 専門用語を使う場合は、必ず簡単な説明を添えること
+- 推測や不確かな情報は断定しないこと
+- コードを出す場合は、コメント付きで分かりやすく書くこと
+
+出力は以下の方針に従ってください。
+- 最初に結論を簡潔に述べる
+- 次に理由や背景を説明する
+- 必要に応じて具体例を示す
+"""
+
 #リクエストボディの型定義
 class ChatRequest(BaseModel):
     message: str
@@ -17,7 +36,10 @@ class ChatRequest(BaseModel):
 def chat(request: ChatRequest):
     response = client.responses.create(
         model="gpt-4o-mini",
-        input=request.message
+        input=[ #旧messages
+            {"role": "system", "content": SYSTEM_PTOMPT},
+            {"role": "user", "content": request.message}
+        ]
     )
     return {
         "reply": response.output_text
